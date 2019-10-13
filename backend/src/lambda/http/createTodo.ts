079e1c0f02593691f85163cebb51778d51c176bb
Max Boyko <maxboykomax@gmail.com>
2019-10-13 00:10:25 -0700

    Initial implementation of the POST endpoint for creating TODOS.

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,31 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid';
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  console.log('Processing event: ', event);
+  const id = uuid.v4();
+  const newTodo: CreateTodoRequest = {
+    id,
+    ...JSON.parse(event.body)
+  };
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newTodo
+  }).promise();
 
   // TODO: Implement creating a new TODO item
-  return undefined
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({ newTodo })
+  }
 }
