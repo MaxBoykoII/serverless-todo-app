522ffb6818666bbd04b3c870c6b5424db55d23b8
Max Boyko <maxboykomax@gmail.com>
2019-11-13 00:07:42 -0800

    fix: deleteTodo handler now properly deletes todo items

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,26 +1,26 @@
 import 'source-map-support/register';
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const todosTable = process.env.TODOS_TABLE;
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId: string = event.pathParameters.todoId
 
-    await docClient.delete({ 
-      TableName: todosTable,
-      Key: {
-        HashKey: 'hashkey',
-        id: todoId
-      }
-    }).promise();
-  
+  await docClient.delete({
+    TableName: todosTable,
+    Key: {
+      todoId
+    }
+  }).promise();
+
   return {
     statusCode: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
     body: JSON.stringify({})
   }
-}
+});
+
+handler.use(cors());
