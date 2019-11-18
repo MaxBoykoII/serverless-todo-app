2eed64d67f8e0b842121eb2866f0e3a04c058ff6
Max Boyko <maxboykomax@gmail.com>
2019-11-17 16:50:58 -0800

    feat: Implement the updateTodo lambda function

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,40 @@
-import 'source-map-support/register'
+import 'source-map-support/register';
 
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest';
 
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+const docClient = new AWS.DynamoDB.DocumentClient();
+const todosTable = process.env.TODOS_TABLE;
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId;
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
+
+  await docClient.update({
+    TableName: todosTable,
+    Key: { todoId },
+    UpdateExpression: `SET #name = :name, #dueDate = :dueDate, #done = :done`,
+    ExpressionAttributeValues: {
+      ':name': updatedTodo.name,
+      ':dueDate': updatedTodo.dueDate,
+      ':done': updatedTodo.done
+    },
+    ExpressionAttributeNames: {
+      '#name': 'name',
+      '#dueDate': 'dueDate',
+      '#done': 'done'
+    }
+  }).promise();
+
+  return {
+    statusCode: 204,
+    body: null
+  }
+});
+
+handler.use(cors());
