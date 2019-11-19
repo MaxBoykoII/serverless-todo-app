8c28e5ecc4e92e9be4d1a0d58006f4317077f6d5
Max Boyko <maxboykomax@gmail.com>
2019-11-19 11:54:54 -0800

    feat: The todos returned by getTodos are now scoped to the authenticated user

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,17 +3,26 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
 import * as middy from 'middy';
 import { cors } from 'middy/middlewares';
+import { getToken, parseUserId } from '../../auth/utils';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const todosTable = process.env.TODOS_TABLE;
+const userIdIndex = process.env.USER_ID_INDEX;
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
   console.log('processing event:', event);
-
-  const result = await docClient.scan({
-    TableName: todosTable
+  const token = getToken(event.headers.Authorization);
+  const userId = parseUserId(token);
+  console.log('parsed user id:', userId);
+  const result = await docClient.query({
+    TableName: todosTable,
+    IndexName: userIdIndex,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
   }).promise();
 
   const items = result.Items;
